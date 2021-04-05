043b7479aa59e96eb6bf65e4885eb682718704bd
zengks <stevevitasks@hotmail.com>
2021-04-05 15:37:59 -0700

    Updates

---
+++ precache-manifest.8ed9593e40ff44a7e832fd6f4094489a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "86b19793a4292d45f5a32efd05ea75ee",
+    "url": "/libraryonline/index.html"
+  },
+  {
+    "revision": "c0f85e9305d22f716ac0",
+    "url": "/libraryonline/static/css/main.f5650d2e.chunk.css"
+  },
+  {
+    "revision": "5b5cbf496bbda3a49067",
+    "url": "/libraryonline/static/js/2.0384977a.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/libraryonline/static/js/2.0384977a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c0f85e9305d22f716ac0",
+    "url": "/libraryonline/static/js/main.bde92871.chunk.js"
+  },
+  {
+    "revision": "1e2396fa60fabd334def",
+    "url": "/libraryonline/static/js/runtime-main.aaecce2e.js"
+  }
+]);
